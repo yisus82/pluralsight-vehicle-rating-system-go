6c238bb17758d546f145e528402c3e0a887d8bdb
yisus82 <djalma8@gmail.com>
2021-04-27 13:40:51 +0200

    Initial setup

---
+++ vehicle.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+
+)
+
+
+// Values array for the feedback.json file
+
+// Model array for the feedback.json file
+
+type rating float32
+
+const (
+	extraPositive rating = 1.2
+	positive      rating = 0.6
+	negative      rating = -0.6
+	initial       rating = 5.0
+	extraNegative rating = -1.2
+)
+
+func init() {
+
+/*
+	inventory = []vehicle{
+		bike{"FTR 1200", "Indian"},
+		bike{"Iron 1200", "Harley"},
+		car{"Sonata", "Hyundai", "Sedan"},
+		car{"SantaFe", "Hyundai", "SUV"},
+		car{"Civic", "Honda", "Hatchback"},
+		car{"A5", "Audi", "Coupe"},
+		car{"Mazda6", "Mazda", "Sedan"},
+		car{"CRV", "Honda", "SUV"},
+		car{"Camry", "Toyota", "Sedan"},
+		truck{"F-150", "Ford", "Truck"},
+		truck{"RAM1500", "Dodge", "Truck"}}
+*/
+//	vehicleResult = make(map[string]feedbackResult)
+
+}
+
+func main() {
+
+	// Generate ratings for the different vehicles
+	
+	// Print ratings for the different vehicles
+}
+
+/*
+func readJSONFile() Values {
+	jsonFile, err := os.Open("feedback.json")
+
+	if err != nil {
+		log.Fatal("File not found")
+	}
+
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var content Values
+	json.Unmarshal(byteValue, &content)
+
+	return content
+}
+*/
